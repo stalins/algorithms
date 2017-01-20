f69b9993145f0ee44ea96d0f81721e1ce2f5b190
Stalin <stalintechnologies.blogspot@gmail.com>
2017-01-19 16:05:25 -0800

    Add files via upload

--- coin.c
+++ coin.c
@@ -7,16 +7,21 @@ int min(int a, int b) {
 	return a<b?a:b;
 }
 
-int pre = INT_MAX;
+/*
 
+	amt = Change required for the amount
+	coins = Array of denomination
+	index = last index of the coin array
+	n = coins count; initially it will be 0
+		this actualy counts the depth of the recursion tree, 
+		less depth is less coin used to provide change
+
+*/
 int coin(int amt, int coins[], int index, int n) {
 
 
-	if (amt == 0) {
-
-		printf("index = %d,  n = %d\n",index, n);
-		return n;
-	}
+	if (amt == 0) 
+			return n;
 
 	if (index < 0 || amt < 0) return INT_MAX;
 
@@ -24,7 +29,6 @@ int coin(int amt, int coins[], int index, int n) {
 	int r = coin(amt, coins, index-1, n);
 
 	int res = min(l, r);
-
 
 	return res;	
 }
